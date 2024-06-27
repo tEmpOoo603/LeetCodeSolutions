a1a10b306470cfb636fc3285a19fe2500262dd94
Efim Aniskin <efimaniskin@gmail.com>
2024-06-27 13:08:38 +0300

    Add tests for Remove Duplicates from Sorted Array II

--- leet_code_tests.py
+++ leet_code_tests.py
@@ -1,6 +1,8 @@
 from roman_to_integer import romanToInt
 from longest_common_prefix import longestCommonPrefix
 from valid_parentheses import isValid
+from remove_duplicates_from_sorted_array_II import removeDuplicates
+
 """roman_to_integer"""
 def test1_romanToInt():
     res = romanToInt("III")
@@ -16,7 +18,6 @@ def test3_romanToInt():
 def test1_longestCommonPrefix():
     res = longestCommonPrefix(["flower","flow","flight"])
     assert "fl"
-
 
 def test2_longestCommonPrefix():
     res = longestCommonPrefix(["dog","racecar","car"])
@@ -43,3 +44,13 @@ def test3_valid_parentheses():
     res = isValid("(]")
     assert res == False
 
+"""remove_duplicates_from_sorted_array_II"""
+def test1_remove_duplicates_from_sorted_array_II():
+    res = removeDuplicates([0,0,1,1,1,1,2,3,3])
+    assert [0,0,1,1,2,3,3]
+def test2_remove_duplicates_from_sorted_array_II():
+    res = removeDuplicates([1,1,1,2,2,3])
+    assert [1,1,2,2,3]
+def test3_remove_duplicates_from_sorted_array_II():
+    res = removeDuplicates([0,0,0,0])
+    assert [0,0]
